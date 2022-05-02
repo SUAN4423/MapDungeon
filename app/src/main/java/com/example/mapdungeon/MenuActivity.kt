4ce00946468a94d4499c78019b09ebd96564c4bb
ellen7788 <erule159motto@gmail.com>
2022-05-02 17:51:36 +0900

    introduce view binding

--- app/src/main/java/com/example/mapdungeon/MenuActivity.kt
+++ app/src/main/java/com/example/mapdungeon/MenuActivity.kt
@@ -2,10 +2,15 @@ package com.example.mapdungeon
 
 import android.app.Activity
 import android.os.Bundle
+import com.example.mapdungeon.databinding.ActivityMenuBinding
 
 class MenuActivity : Activity() {
+    private lateinit var menuBinding: ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        menuBinding = ActivityMenuBinding.inflate(layoutInflater)
+        val view = menuBinding.root
+        setContentView(view)
     }
 }
