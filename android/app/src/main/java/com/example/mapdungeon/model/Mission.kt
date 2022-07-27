564be1840f21a956c93813e1f8f324342b0c914b
SUAN4423 <38971033+SUAN4423@users.noreply.github.com>
2022-07-27 18:05:56 +0900

    address

--- android/app/src/main/java/com/example/mapdungeon/model/Mission.kt
+++ android/app/src/main/java/com/example/mapdungeon/model/Mission.kt
@@ -1,15 +1,17 @@
 package com.example.mapdungeon.model
 
 
+import android.location.Address
+import com.example.mapdungeon.cityname.AddressMap
 import java.time.ZonedDateTime
 import java.util.*
 
 data class Mission(
+    val missionChar: Char,
     val id: String = UUID.randomUUID().toString(),
     var isClear: Boolean = false,
-    val missionChar: Char,
     val createdAt: Date = Date(),
-    var clearedAt: ZonedDateTime? = null
+    var clearedAt: Date? = null,
+    var address: AddressMap? = null
 ) {
-
 }
